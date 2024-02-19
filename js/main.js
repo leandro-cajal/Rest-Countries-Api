d0b3a10a310d274fe72ca14a73f18287532c4ca8
leandro-cajal <leo_origin@hotmail.com>
2024-02-19 13:53:00 -0300

    filter functions added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+const FILTER_OPTIONS = document.querySelectorAll(".option");
+const TOGGLE_MENU = document.querySelector("#toggle-menu");
+const OPTIONS_MENU = document.querySelector("#options-menu");
+const MAIN_CONTAINER = document.querySelector("#main-container");
+const SEARCH_BAR = document.querySelector("#search");
+
 async function fetchedData() {
     const url = "./data.json";
     try {
@@ -12,23 +18,42 @@ async function fetchedData() {
     }
 }
 
-const MAIN_CONTAINER = document.querySelector("#main-container");
-
 document.addEventListener("DOMContentLoaded", async () => {
     const data = await fetchedData();
-    console.log(data[10]);
     renderCountries(data);
-    
+    SEARCH_BAR.addEventListener("keyup", (event)=>{
+        const SEARCH_INPUT = event.target.value.trim().toLowerCase();
+        const result = filterByName(data, SEARCH_INPUT);
+        renderCountries(result);
+    });
+    FILTER_OPTIONS.forEach(option =>{
+        option.addEventListener("click",() =>{
+            const OPTION_VALUE = option.dataset.value;
+            const result = filterByRegion(data, OPTION_VALUE);
+            renderCountries(result);
+        })
+    });
 });
 
+function filterByName(countries,searchTerm){
+    let NEW_ARRAY = countries.filter(country => country.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    return NEW_ARRAY;
+}
+
+function filterByRegion(countries,optionValue){
+    let NEW_ARRAY = countries.filter(country => country.region.toLowerCase() === optionValue);
+    return NEW_ARRAY;
+}
 
 function renderCountries(COUNTRIES) {
+    MAIN_CONTAINER.innerHTML = '';
     const ul = document.createElement("ul");
     ul.classList = "country-list";
     ul.innerHTML = "";
     COUNTRIES.forEach(country => {
         const li = document.createElement("li");
         li.classList = "country";
+        fixedNumber = fixNumberPopulation(country.population);
         li.innerHTML = `
         <div class="card">
             <div class="card-container-img">
@@ -36,14 +61,23 @@ function renderCountries(COUNTRIES) {
             </div>
             <article class="card-info">
                 <h2 class="card-info-title">${country.name}</h2>
-                <div class="card-info-text"><h4 class="font-semibold">Population: </h4><p>${country.population}</p></div>
+                <div class="card-info-text"><h4 class="font-semibold">Population: </h4><p>${fixedNumber}</p></div>
                 <div class="card-info-text"><h4 class="font-semibold">Region: </h4><p>${country.region}</p></div>
                 <div class="card-info-text"><h4 class="font-semibold">Capital: </h4><p>${country.capital}</p></div>
             </article>
         </div>
         `;
-        console.log(`${country.flags.svg}`);
         ul.appendChild(li);
     });
-    MAIN_CONTAINER.appendChild(ul)
+    MAIN_CONTAINER.appendChild(ul);
 }
+
+function fixNumberPopulation(number){
+        return number.toLocaleString('es-ES');
+}
+
+TOGGLE_MENU.addEventListener('click', () => {
+    OPTIONS_MENU.classList.toggle('hidden');
+    TOGGLE_MENU.classList.toggle('rotate');
+});
+
